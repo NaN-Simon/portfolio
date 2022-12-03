f6f151030ab1da364c4b0379ac5a748864e99856
NaN-Simon <goosaimon@gmail.com>
2022-12-03 11:18:11 +0300

    feat: change file-scan - write into log file

--- projects/file-scan/index.js
+++ projects/file-scan/index.js
@@ -1,22 +1,25 @@
 /* RUN: put index.js file in scan zone,
         enter: node index.js
+        read log.txt
 */
 let fs = require("fs");
 const path = require('path');
+let arr = []
+
 function readFolder(folderPath){
   const readdirCallback = fs.readdirSync(folderPath, { withFileTypes: true });
 
   readdirCallback.forEach((dirEntry) => {
     if(!dirEntry.isDirectory()){
-      console.log(path.join(folderPath, path.basename(dirEntry.name, '.txt')))
+      const file = path.join(folderPath, path.basename(dirEntry.name, '.txt'))
+      arr.push(file);
     } else {
-          readFolder(path.join(folderPath, dirEntry.name))
-        }
-      });
+      readFolder(path.join(folderPath, dirEntry.name))
+    }
+  });
+  fs.writeFileSync("log.txt", arr.join('\n'));
       return folderPath
 }
 
-
-// readFolder(path.resolve('..'));
 readFolder('.');
 
